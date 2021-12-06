5d65614b07db3ad51b5e3d08ead9d63c78107764
yuuki-katsuta <reptpr0314@gmail.com>
2021-12-06 23:24:11 +0900

    メモ機能実装

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import { useState, ChangeEvent, FC } from 'react';
+import styled from 'styled-components';
 
-function App() {
+export const App: FC = () => {
+  const [text, setText] = useState<string>('');
+  const [memos, setMemos] = useState<string[]>([]);
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  const onClickAdd = () => {
+    const newMemos = [...memos];
+    newMemos.push(text);
+    setMemos(newMemos);
+    setText('');
+  };
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
   return (
     <div>
-      <p>hello, world</p>
+      <h1>かんたんメモアプリ</h1>
+      <input type='text' value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SmemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => onClickDelete(index)}>削除</SButton>
+              </SmemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
     </div>
   );
-}
-
-export default App;
+};
+const SButton = styled.button`
+  margin-left: 16px;
+`;
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding: 16px;
+  margin: 8px;
+`;
+const SmemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
