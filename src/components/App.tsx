81cd39ae579fb7b41edb4b087cb3b1c2672ffe30
yuuki-katsuta <reptpr0314@gmail.com>
2021-12-08 21:02:02 +0900

    カスタムフック化

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,21 @@
 import { useState, ChangeEvent, FC, useCallback } from 'react';
 import styled from 'styled-components';
 import { MemoList } from './MemoList';
+import { useMemoList } from '../hooks/useMemoList';
 
 export const App: FC = () => {
+  const { memos, addTodo, deleteTodo } = useMemoList();
   const [text, setText] = useState<string>('');
-  const [memos, setMemos] = useState<string[]>([]);
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
   const onClickAdd = () => {
-    const newMemos = [...memos];
-    newMemos.push(text);
-    setMemos(newMemos);
+    addTodo(text);
     setText('');
   };
   const onClickDelete = useCallback(
-    (index: number) => {
-      const newMemos = [...memos];
-      newMemos.splice(index, 1);
-      setMemos(newMemos);
-    },
-    [memos]
+    (index: number) => deleteTodo(index),
+    [deleteTodo]
   );
   return (
     <div>
